b9ae084efdea1476506105e8b27e7276eb23d831
martkir <martinbarut@hotmail.com>
2019-09-10 14:11:49 +0100

    fixed the config dict

--- experiments.py
+++ experiments.py
@@ -1,24 +1,19 @@
 import setup.lstm.engines
 
-"""
-experiment ideas:
-- SGD vs. Adam. (which is better).
-"""
-
 
 def train_vanilla_lstm():
+
     config = {
         'dataset_name': 'wikitext-2',
-        'batch_size': 20,
         'chunk_size': 35,
         'skip_size': 35,
-        'learning_rate': 0.001,
+        'batch_size': 20,
+        'input_size': 64,  # size of word embeddings.
+        'hidden_size': 32,
+        'n_layers': 1,
+        'lr': 0.001,  # learning rate
         'num_epochs': 2,
-        'model': {
-            'input_size': 64,  # size of embeddings.
-            'hidden_size': 32,
-            'n_layers': 1
-        },
+        'experiment_dirname': 'results/vanilla_lstm',  # where checkpoints and training/ validation results are saved.
     }
 
     engine = setup.lstm.engines.Engine(config)
